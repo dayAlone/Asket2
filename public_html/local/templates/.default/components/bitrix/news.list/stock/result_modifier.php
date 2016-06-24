7fa52bb2c71d0a9f291d199ae912cf3e371866a9
dayAlone <andrey.slider@gmail.com>
2016-06-24 23:12:29 +0700

    fix

--- public_html/local/templates/.default/components/bitrix/news.list/stock/result_modifier.php
+++ public_html/local/templates/.default/components/bitrix/news.list/stock/result_modifier.php
@@ -5,7 +5,7 @@ foreach ($arResult['ITEMS'] as $key=>$item) $ids[] = $item['IBLOCK_SECTION_ID'];
 $raw = CIBlockSection::GetList(Array($by=>$order), Array('IBLOCK_ID'=>$arParams['IBLOCK_ID'], 'ID'=>$ids), true, array('ID', 'NAME', 'CODE'));
 while($section = $raw->Fetch()) $arResult['SECTIONS'][$section['ID']] = $section;
 if (!function_exists('sections_id_sort')) {
-    function ids_sort($a, $b) { return ($a['IBLOCK_SECTION_ID'] <= $b['IBLOCK_SECTION_ID']) ? -1 : 1; }
+    function sections_id_sort($a, $b) { return ($a['IBLOCK_SECTION_ID'] <= $b['IBLOCK_SECTION_ID']) ? -1 : 1; }
 }
 uasort($arResult['ITEMS'], "sections_id_sort");
 ?>
