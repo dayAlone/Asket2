c073fa95a4d2be328ad210457f6fa590a103d7c7
dayAlone <andrey.slider@gmail.com>
2016-06-28 19:00:16 +0700

    CSS

--- public_html/local/templates/.default/components/bitrix/catalog.section.list/buttons/template.php
+++ public_html/local/templates/.default/components/bitrix/catalog.section.list/buttons/template.php
@@ -1,19 +1,19 @@
 <?if (!defined("B_PROLOG_INCLUDED") || B_PROLOG_INCLUDED!==true)die();
 $this->setFrameMode(true);
 if(count($arResult['SECTIONS']) > 1):?>
-	<?foreach ($arResult['SECTIONS'] as $key => &$item):?>
+    <?foreach ($arResult['SECTIONS'] as $key => &$item):?>
     <div class="buttons__item">
         <div class="buttons__content">
             <div class="buttons__title"><?=$item['NAME']?></div>
             <div class="buttons__links">
-				<?foreach ($item['CHILD'] as $el):?>
-					<a href="/stock/#<?=$item['CODE']?>__<?=$el['CODE']?>">
-						<span><?=$el['NAME']?></span>
-					</a>
-				<?endforeach;?></div>
+                <?foreach ($item['CHILD'] as $el):?>
+                    <a href="/stock/#<?=$item['CODE']?>__<?=$el['CODE']?>">
+                        <span><?=$el['NAME']?></span>
+                    </a>
+                <?endforeach;?></div>
         </div>
     </div>
     <!--<a href="<?=$item['SECTION_PAGE_URL']?>" class="buttons__item"><span class="buttons__text"><?=html_entity_decode($item['NAME'])?></span></a>-->
-  	<?endforeach;?>
+    <?endforeach;?>
 
 <?endif;?>
