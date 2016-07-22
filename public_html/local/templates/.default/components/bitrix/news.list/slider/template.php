85798bbe5f70c438ebfe777c87de208ef0ead092
dayAlone <andrey.slider@gmail.com>
2016-07-22 19:32:53 +0700

    Fix

--- public_html/local/templates/.default/components/bitrix/news.list/slider/template.php
+++ public_html/local/templates/.default/components/bitrix/news.list/slider/template.php
@@ -2,7 +2,7 @@
     <?foreach ($arResult['ITEMS'] as $key=>$item):?>
     <div class="slider__item">
         <div class="slider__content">
-            <div class="slider__title"><?=$item['~NAME']?></div>
+            <div class="slider__title"><span><?=$item['~NAME']?></span></div>
             <div class="slider__text"><?=$item['~PREVIEW_TEXT']?></div>
             <?if (strlen($item['PROPERTIES']['BUTTON_LINK']['VALUE']) > 0 && strlen($item['PROPERTIES']['BUTTON_TEXT']['VALUE']) > 0):?>
                 <a href="<?=$item['PROPERTIES']['BUTTON_LINK']['VALUE']?>" class="slider__button"><?=$item['PROPERTIES']['BUTTON_TEXT']['VALUE']?></a>
